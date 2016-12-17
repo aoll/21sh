ee63cf4e9cefa6665cb742ee9c1d2bbe5f8007d4
aoll <agollivier@gmail.com>
2016-12-17 17:33:47 +0100

    add char and restore cursor

--- src/cursor/ft_cursor_add_char.c
+++ src/cursor/ft_cursor_add_char.c
@@ -195,20 +195,65 @@ int  ft_cursor_print_overide_line(t_cursor *cursor, t_arr *arr)
   return (EXIT_SUCCESS);
 }
 
+
+/**
+ * print the down line
+ */
+int  ft_cusor_clear_down_line(t_cursor *cursor, t_arr *arr)
+{
+  int index;
+  int pos_y;
+  int i;
+
+  // ft_term_apply_cmd(cursor->mode_insertion_end, 1);
+  index = cursor->index_line;
+  cursor->index_line--;
+  ft_cursor_home(cursor, arr);
+  ft_term_apply_cmd(cursor->save_cursor_position, 1);
+  pos_y = cursor->pos_y - cursor->y_start;
+  ft_term_apply_cmd(cursor->clear_current_line, 1);
+  if (cursor->y_total > 1)
+  {
+    ft_term_apply_cmd(cursor->left, cursor->prompt_len);
+    ft_term_apply_cmd(cursor->down, 1);
+    i = 1;
+    while (i < cursor->y_total && i < cursor->terminal_size.ws_row)
+    {
+      ft_term_apply_cmd(cursor->clear_current_line, 1);
+      if (i < cursor->y_total - 1 && i < cursor->terminal_size.ws_row - 1)
+        ft_term_apply_cmd(cursor->down, 1);
+      i++;
+    }
+  }
+  ft_term_apply_cmd(cursor->restore_cursor_position, 1);
+  ft_arr_print(arr);
+  cursor->index_line = arr->length;
+  ft_cursor_restore_y_x(cursor, arr, ft_cursor_nb_line_displayed(cursor, arr, 0, 0));
+  ft_cursor_restore_index(cursor, arr, index);
+  // while (cursor->index_line != index)
+  // {
+  //   ft_cursor_left(cursor, arr);
+  // }
+
+  return (EXIT_SUCCESS);
+}
+
 /**
  * add the new elem to arr and print
  */
 int  ft_add_char_isprint(t_cursor *cursor, t_arr *arr, char c)
 {
-  ft_arr_add_char(cursor, arr, c, 0);
   if (cursor->index_line == (int)arr->length)
   {
+    ft_arr_add_char(cursor, arr, c, 0);
     ft_cursor_print_char(cursor, c, arr);
   }
   else
   {
-    ft_cursor_print_char(cursor, c, arr);
-    ft_cursor_print_overide_line(cursor, arr);
+    ft_arr_add_char(cursor, arr, c, 0);
+    ft_cusor_clear_down_line(cursor, arr);
+    // ft_cursor_print_char(cursor, c, arr);
+    // ft_cursor_print_overide_line(cursor, arr);
   }
   return (EXIT_SUCCESS);
 }
