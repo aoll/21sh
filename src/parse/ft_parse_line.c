129693178ccac906200a8403df55936f2780bc50
aoll <agollivier@gmail.com>
2016-12-23 09:26:44 +0100

    parsing, split the line with ;, pop and free usless space

--- src/parse/ft_parse_line.c
+++ src/parse/ft_parse_line.c
@@ -1,9 +1,11 @@
 #include "project.h"
 
 /**
- * Parsing section
+ * separate the line on a multiple line whitout "free ;" (; not inside quote or
+ * dquote)
+ * return null if lien lenght == 0
  */
-int  ft_parse_line(t_arr *arr)
+t_arr  *ft_parse_separate_cmd(t_arr *arr)
 {
   t_arr *tab_cmds;
   t_arr *cmd;
@@ -14,9 +16,8 @@ int  ft_parse_line(t_arr *arr)
 
   if (!arr->length)
   {
-    return (EXIT_SUCCESS);
+    return (NULL);
   }
-  ft_putstr("\n");
   index = 0;
   dquote = false;
   quote = false;
@@ -33,7 +34,6 @@ int  ft_parse_line(t_arr *arr)
     {
       dquote = !dquote;
     }
-
     if (*s_line == '\'' && !dquote)
     {
       quote = !quote;
@@ -45,12 +45,132 @@ int  ft_parse_line(t_arr *arr)
       cmd->f_del = &ft_memdel;
       cmd->f_print = &ft_arr_putchar;
     }
-    else
+    else if (*s_line != ';')
     {
       ft_arr_push(&cmd, ft_strdup((const char *)s_line), -1);
     }
     index++;
   }
+  return (tab_cmds);
+}
+
+/**
+ * pop usless space outside quote or dquote
+ */
+int  ft_parse_pop_space_inside(t_arr *arr)
+{
+  int index;
+  unsigned char *s_line;
+  bool dquote;
+  bool quote;
+
+  index = 0;
+  while (index < (int)arr->length)
+  {
+    s_line = (unsigned char *)arr->ptr + index * arr->sizeof_elem;
+    s_line = *(unsigned char **)s_line;
+    if (*s_line == '"' && !quote)
+    {
+      dquote = !dquote;
+    }
+    if (*s_line == '\'' && !dquote)
+    {
+      quote = !quote;
+    }
+    if (!quote && !dquote && ft_isspace(*s_line))
+    {
+      if (index + 1 < (int)arr->length)
+      {
+        s_line = (unsigned char *)arr->ptr + (index + 1) * arr->sizeof_elem;
+        s_line = *(unsigned char **)s_line;
+        if (ft_isspace(*s_line))
+        {
+          free(ft_arr_pop(&arr, index));
+        }
+        else
+        {
+          index++;
+        }
+      }
+      else
+      {
+        free(ft_arr_pop(&arr, index));
+      }
+    }
+    else
+    {
+      index++;
+    }
+  }
+  return (EXIT_SUCCESS);
+}
+
+/**
+ * pop usles space on the begin of the command
+ */
+int  ft_parse_pop_prev_space(t_arr *arr)
+{
+  int index;
+  unsigned char *s_line;
+
+  index = 0;
+  while (index < (int)arr->length)
+  {
+    s_line = (unsigned char *)arr->ptr + index * arr->sizeof_elem;
+    s_line = *(unsigned char **)s_line;
+    if (!ft_isspace(*s_line))
+    {
+      return (EXIT_SUCCESS);
+    }
+    else
+    {
+      free(ft_arr_pop(&arr, 0));
+    }
+  }
+
+  return (EXIT_SUCCESS);
+}
+
+/**
+ * itere every line and aply function who pop every usless space
+ */
+int  ft_parse_pop_space(t_arr *tab_cmds)
+{
+  int index;
+  t_arr *cmd;
+
+  index = 0;
+  while (index < (int)tab_cmds->length)
+  {
+    cmd = *(t_arr **)((unsigned char *)tab_cmds->ptr + index * tab_cmds->sizeof_elem);
+    ft_parse_pop_prev_space(cmd);
+    ft_parse_pop_space_inside(cmd);
+    if (!cmd->length)
+    {
+      ft_arr_free(ft_arr_pop(&tab_cmds, index));
+    }
+    index++;
+  }
+
+  return (EXIT_SUCCESS);
+}
+
+
+/**
+ * parsing
+ */
+int  ft_parse_line(t_arr *arr)
+{
+  t_arr *tab_cmds;
+  int index;
+  t_arr *cmd;
+
+  if (!(tab_cmds = ft_parse_separate_cmd(arr)))
+  {
+    return (EXIT_SUCCESS);
+  }
+  ft_parse_pop_space(tab_cmds);
+  ft_putstr("\n");
   index = 0;
   while (index < (int)tab_cmds->length)
   {
