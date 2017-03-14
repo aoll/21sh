2a699d69a04b0797d9045bcc3c3d2f2fb55a547e
Alexandre OLLIVIER <aollivie@e1r5p7.42.fr>
2017-03-14 14:42:32 +0100

    norme

--- src/norme.sh
+++ src/norme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-file='ft_env_init.c'
-#perl -p -i -e 's/  /\t/g' $file #remplace 2 espace par des tabulation ne doit pas prendre le header
+file=$1
+perl -p -i -e  's/  /\t/g' $file #remplace 2 espace par des tabulation ne doit pas prendre le header
+#perl -p -i -e  's/[ \t]*$//' $file efface les espaces en fin de ligne
 #perl -p -i -e 's/return;/return ;/g' $file #remplace return; par return ;
 
