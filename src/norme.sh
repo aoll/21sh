7e5a92605a012208c7c49f69e2d3aff380101700
Alexandre OLLIVIER <aollivie@e1r5p7.42.fr>
2017-03-14 16:00:27 +0100

    script

--- src/norme.sh
+++ src/norme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file=$1
-perl -p -i -e  's/  /\t/g' $file #remplace 2 espace par des tabulation ne doit pas prendre le header
+sed -i .back  '12,1000s/  /	/g'  $file #remplace 2 espace par des tabulation ne doit pas prendre le header
 #perl -p -i -e  's/[ \t]*$//' $file efface les espaces en fin de ligne
 #perl -p -i -e 's/return;/return ;/g' $file #remplace return; par return ;
 
