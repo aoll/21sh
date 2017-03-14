bb7e90a87c4d682e2a3b1aa953f7098a9ac4337e
Alexandre OLLIVIER <aollivie@e1r5p7.42.fr>
2017-03-14 11:53:16 +0100

    norme

--- src/cursor/ft_arr_add_char.c
+++ src/cursor/ft_arr_add_char.c
@@ -3,34 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_arr_add_char.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: aollivie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/12 17:14:44 by alex              #+#    #+#             */
-/*   Updated: 2017/03/12 17:15:03 by alex             ###   ########.fr       */
+/*   Created: 2017/03/14 11:51:54 by aollivie          #+#    #+#             */
+/*   Updated: 2017/03/14 11:51:57 by aollivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "project.h"
+
 /*
 ** add a char to arr a marke if is a element to a tabulation
 */
-int  ft_arr_add_char(t_cursor *cursor, t_arr *arr, char c, int is_tab)
-{
-  char *char_line;
 
-  if (c == '"' && !cursor->quote)
-  {
-    cursor->dquote = !cursor->dquote;
-  }
-  if (c == '\'' && !cursor->dquote)
-  {
-    cursor->quote = !cursor->quote;
-  }
-  char_line = ft_strnew(8);
-  ft_bzero(char_line, 8);
-  *char_line = c;
-  char_line[4] = is_tab;
-  ft_arr_push(arr, char_line, cursor->index_line);
-  cursor->index_line++;
-  return (EXIT_SUCCESS);
+int	ft_arr_add_char(t_cursor *cursor, t_arr *arr, char c, int is_tab)
+{
+	char *char_line;
+
+	if (c == '"' && !cursor->quote)
+	{
+		cursor->dquote = !cursor->dquote;
+	}
+	if (c == '\'' && !cursor->dquote)
+	{
+		cursor->quote = !cursor->quote;
+	}
+	char_line = ft_strnew(8);
+	ft_bzero(char_line, 8);
+	*char_line = c;
+	char_line[4] = is_tab;
+	ft_arr_push(arr, char_line, cursor->index_line);
+	cursor->index_line++;
+	return (EXIT_SUCCESS);
 }
