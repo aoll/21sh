e070232e50aea6470c082683d149709eba0c2f13
aoll <agollivier@gmail.com>
2017-04-23 21:57:46 +0200

    arr cmd

--- src/parse/ft_parse_init_new_arr_line.c
+++ src/parse/ft_parse_init_new_arr_line.c
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/05 18:31:05 by alex              #+#    #+#             */
-/*   Updated: 2017/02/05 18:46:54 by alex             ###   ########.fr       */
+/*   Updated: 2017/04/23 20:35:22 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	ft_parse_init_new_arr_line(t_arr **arr)
 {
 	t_arr			*cmd;
 
-	cmd = *arr;
 	cmd = ft_arr_new(1, sizeof(char *));
 	cmd->f_del = &ft_memdel;
 	cmd->f_print = &ft_arr_putchar;
